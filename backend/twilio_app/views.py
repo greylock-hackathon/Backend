bdf484bc8202ac0a0b408b6730dcf66b1e70f2a6
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-07-09 16:20:56 -0700

    send correctly formatted response to twilio

--- backend/twilio_app/views.py
+++ backend/twilio_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from twilio import twiml
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,4 +11,6 @@ def index(request):
 @csrf_exempt
 def new_message(request):
     print request.POST
-    return HttpResponse('ok')
+    r = twiml.Response()
+    r.say(request.POST['Body'])
+    return HttpResponse(str(r))
