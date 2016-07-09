3435c36fcf078773fccbfeab328be3f2f635a629
Nipun Singh <ns2se@virginia.edu>
2016-07-09 13:52:14 -0700

    Added in uber key and 1st step of uber authentication flow

--- backend/uber_integration/urls.py
+++ backend/uber_integration/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'sandbox',views.sandbox,name='sandbox'),
+    url(r'authenticate',views.authenticate,name='authenticate'),
 ]
--- backend/uber_integration/views.py
+++ backend/uber_integration/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from rauth import OAuth2Service
+import sys
+import requests
 
 def index(request):
     return HttpResponse("Hello, world. You're at the uber integration index.")
@@ -11,3 +14,25 @@ def index(request):
 #sandbox where we test different uber related services
 def sandbox(request):
     return render(request, 'uber_integration/sandbox.html', {})
+
+def get_key(key_num):
+    file = open('../../keys.txt', 'r')
+    uber_key = file.read()
+    print >> sys.stderr, uber_key
+    return uber_key
+
+def authenticate(request):
+    uber_api = OAuth2Service(
+    client_id='vvXhhV_qjiVbwcADOtvVjCK2aj91RJJd',
+    client_secret= 'NX_tpqkReG6xzzA2JA26867pq5XLc7y4GWGSkFwn', #get_key(0), #'INSERT_CLIENT_SECRET',
+    name='Greylock Hackfest',
+    authorize_url='https://login.uber.com/oauth/authorize',
+    access_token_url='https://login.uber.com/oauth/token',
+    base_url='https://api.uber.com/v1/',
+)
+
+    parameters = {'response_type': 'code','redirect_uri': 'https://www.google.com/','scope': 'profile'}
+
+	#### Redirect user here to authorize your application
+    login_url = uber_api.get_authorize_url(**parameters)
+    return HttpResponse("Succesful Authentication" + login_url)
