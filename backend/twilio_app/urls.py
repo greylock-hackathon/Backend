2a10817e8fbe4399d957db0de4005137e235ee96
kelvinabrokwa <kelvinabrokwa@gmail.com>
2016-07-09 15:02:21 -0700

    more twilio endpoint

--- backend/twilio_app/urls.py
+++ backend/twilio_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^new_message$', views.new_message, name='new_message')
 ]
