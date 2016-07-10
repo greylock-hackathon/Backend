bf4506f767c740fa86e0e416651e79ea0a1f273d
Nipun Singh <ns2se@virginia.edu>
2016-07-10 01:14:45 -0700

    Formatted lat/lng/name json info into array to be sent via SMS to Android

--- backend/explore/places.py
+++ backend/explore/places.py
@@ -25,6 +25,16 @@ def get_places(lng, lat):
         'id': place.place_id
     } for place in query_result.places]
 
+def encode_places(places):
+    encoded_places = []
+    for place in places[:4]:
+        encoded_places.append(str(place['lat'])[:7])
+        encoded_places.append(str(place['lng'])[:7])
+        encoded_places.append(place['name'][:30])
+    return encoded_places
+
 if __name__ == '__main__':
     places = get_places(-122.1430, 37.4419)
     print(places)
+    print '======='
+    print encode_places(places)
